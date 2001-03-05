904a446611af3205d158041b9cb1c206fc8db0a4
hns <hannesw@gmail.com>
2001-03-05 16:11:25 +0000

    inital check-in of new parent info class

---
+++ src/helma/objectmodel/ParentInfo.java
@@ -0,0 +1,41 @@
+// ParentInfo.java
+// Copyright (c) Hannes Wallnöfer 1999-2000
+
+package helma.objectmodel;
+
+
+/**
+ *  This class describes a parent relation between releational nodes.
+ */
+
+public class ParentInfo {
+
+    public final String propname;
+    public final String virtualname;
+    public final boolean named;
+    public final boolean isroot;
+
+
+    public ParentInfo (String desc) {
+	int n = desc.indexOf ("[named]");
+	named = n > -1;
+	String d = named ? desc.substring (0, n) : desc;
+
+	int dot = d.indexOf (".");
+	if (dot > -1) {
+	    propname = d.substring (0, dot).trim();
+	    virtualname = d.substring (dot+1).trim();
+	} else {
+	    propname = d.trim();
+	    virtualname = null;
+	}
+	
+	isroot = "root".equals (propname);
+	System.err.println ("created "+this);
+    }	
+
+    public String toString () {
+	return "ParentInfo["+propname+","+virtualname+","+named+"]";
+    }
+
+}
