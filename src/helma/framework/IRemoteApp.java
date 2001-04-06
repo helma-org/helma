a336ab595529c3d9c98a873962239da34fca02f8
hns <hannesw@gmail.com>
2001-04-06 17:45:52 +0000

    added replicateCache() method for cache replication

--- src/helma/framework/IRemoteApp.java
+++ src/helma/framework/IRemoteApp.java
@@ -4,6 +4,7 @@
 package helma.framework;
 
 import java.rmi.*;
+import java.util.Vector;
 
 /**
  * RMI interface for an application. Currently only execute is used and supported.
@@ -13,7 +14,7 @@ public interface IRemoteApp extends Remote {
 
     public ResponseTrans execute (RequestTrans param) throws RemoteException;
 
-    public ResponseTrans get (String path, String sessionID) throws RemoteException;
+    public void replicateCache (Vector add, Vector delete) throws RemoteException;
 
     public void ping () throws RemoteException;
  
