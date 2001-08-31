4da2834955d49bed4a39b15dc8d72eedee896a3d
hns <hannesw@gmail.com>
2001-08-31 12:06:15 +0000

    moved from helma.mime to helma.util.mime package to reduce number of top level packages.

--- src/helma/util/mime/MultipartInputStream.java
+++ src/helma/util/mime/MultipartInputStream.java
@@ -3,7 +3,7 @@
 // (c) COPYRIGHT MIT and INRIA, 1996.
 // Please first read the full copyright statement in file COPYRIGHT.html
 
-package helma.mime;
+package helma.util.mime;
 
 import java.io.* ;
 
