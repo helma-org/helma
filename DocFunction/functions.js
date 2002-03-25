bf86d54c6733b60eb9393604cde4f3e84730f723
stefanp <helma@helma.org>
2002-03-25 18:36:56 +0000

    removed obsolete functions: calling href() on Java objects does indeed work. The problem was that getParentElement() method in Application always returned null (due to a very hard to find bug in helma.main.Server).

--- DocFunction/functions.js
+++ DocFunction/functions.js
@@ -1,17 +1,4 @@
 
-/**
-  * overrides the internal href-function, as
-  * helma.framework.core.Application.getNodeHref(Object,String)
-  * isn't able to compute correct urls for non-node objects.
-  * @arg action of prototype
-  */
-function href(action)	{
-	var url = getProperty("baseURI");
-	url = (url==null || url=="null") ? "" : url;
-	url += this.getParentElement().getParentElement().getName() + "/api/" + this.getParentElement().getName() + "/" + this.getTypeName().toLowerCase()+"_"+this.name + "/" + ( (action!=null && action!="") ? action : "main" );
-	return url;
-}
-
 
 function getApplication()	{
 	return this.getParentElement().getParentElement();
