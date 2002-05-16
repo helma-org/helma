ffa1a142500ad9fe796dea8ec00279a7c4f25f41
stefanp <helma@helma.org>
2002-05-16 00:43:01 +0000

    exceptino to replace com.sleepycat.db.DbException

---
+++ src/helma/objectmodel/DatabaseException.java
@@ -0,0 +1,49 @@
+// DatabaseException.java
+ 
+package helma.objectmodel;
+
+
+/**
+ * Thrown on any kind of Database-Error
+ */
+ 
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException (String msg) {
+	super (msg);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
