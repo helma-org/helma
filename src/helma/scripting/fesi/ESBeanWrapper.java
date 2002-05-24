49abb017bc038cff52b5707ec0c77b86ef4f9b92
stefanp <helma@helma.org>
2002-05-24 13:57:41 +0000

    throw nicer exceptions

--- src/helma/scripting/fesi/ESBeanWrapper.java
+++ src/helma/scripting/fesi/ESBeanWrapper.java
@@ -1,42 +1,54 @@
 package helma.scripting.fesi;
 
-//import helma.framework.core.*;
-//import helma.framework.IPathElement;
-
 import helma.objectmodel.INode;
 import FESI.Interpreter.Evaluator;
 import FESI.Exceptions.EcmaScriptException;
-import FESI.Data.*;
-//import java.util.*;
+import FESI.Data.ESNull;
+import FESI.Data.ESValue;
+import FESI.Data.ESWrapper;
 
+/**
+ * Wrap a Java Bean for use in EcmaScript.
+ */
 
 public class ESBeanWrapper extends ESWrapper {
 
 	FesiEvaluator eval;
 
-	public ESBeanWrapper (Object object, FesiEvaluator eval)	{
+	public ESBeanWrapper (Object object, FesiEvaluator eval) {
 		super (object, eval.getEvaluator());
 		this.eval = eval;
 	}
 
-//    public void putProperty(String propertyName, ESValue propertyValue, int hash) throws EcmaScriptException {
-//	wrapper.putProperty (propertyName, propertyValue, hash);
-//    }
-
-//     public ESValue getProperty (int i) throws EcmaScriptException {
-// 	return wrapper.getProperty (i);
-//     }
-
-    public ESValue getProperty(String propertyName, int hash) throws EcmaScriptException {
-		ESValue val = super.getProperty (propertyName, hash);
-		if( val instanceof ESWrapper && ((ESWrapper)val).getJavaObject() instanceof INode )	{
-			return eval.getNodeWrapper( (INode) ((ESWrapper)val).getJavaObject() );
-		}	else	{
-			return val;
+	/**
+ 	* Wrap getProperty, return ESNode if INode would be returned.
+ 	*/
+    public ESValue getProperty(String propertyName, int hash)
+                                 throws EcmaScriptException {
+		try {
+			ESValue val = super.getProperty (propertyName, hash);
+			if (val instanceof ESWrapper && ((ESWrapper)val).getJavaObject() instanceof INode)	{
+				return eval.getNodeWrapper( (INode) ((ESWrapper)val).getJavaObject() );
+			} else {
+				return val;
+			}
+		} catch (Exception rte) {
+			return ESNull.theNull;
 		}
     }
 
- 
+    public void putProperty(String propertyName, ESValue propertyValue, int hash)
+                              throws EcmaScriptException {
+    	try {
+    		super.putProperty (propertyName, propertyValue, hash);
+    	} catch (Exception rte) {
+    		// create a nice error message
+    		throw new EcmaScriptException("property " + propertyName +
+    		" is readonly on " + getJavaObject().toString() );
+    	}
+	}
+
 }
+
 
 
