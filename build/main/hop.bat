cc637837308c1bd0fdaf2ac2ba47e0ce34a5d7bb
hns <hannesw@gmail.com>
2002-06-04 17:04:33 +0000

    Removed berkeley.jar, openxml.jar and sax.jar from classpath, added crimson.jar.

--- build/main/hop.bat
+++ build/main/hop.bat
@@ -2,8 +2,8 @@
 
 rem Batch file for starting Hop with a JDK-like virtual machine.
 
-set JARS=lib\helma.jar;lib\berkeley.jar;lib\village.jar;lib\jsdk.jar;lib\openxml.jar
-set JARS=%JARS%;lib\sax.jar;lib\regexp.jar;lib\netcomponents.jar;lib\jimi.jar
+set JARS=lib\helma.jar;lib\crimson.jar;lib\village.jar;lib\jsdk.jar
+set JARS=%JARS%;lib\regexp.jar;lib\netcomponents.jar;lib\jimi.jar
 set JARS=%JARS%;lib\mail.jar;lib\activation.jar;lib\mysql.jar;lib\jdom.jar;lib\minml.jar
 
 set HOP_PORT=8080
--- hop.sh
+++ hop.sh
@@ -4,8 +4,8 @@
 
 export HOP_PORT=8080 
 
-export JARS=lib/helma.jar:lib/berkeley.jar:lib/village.jar:lib/jsdk.jar:lib/openxml.jar
-export JARS=$JARS:lib/sax.jar:lib/regexp.jar:lib/netcomponents.jar:lib/jimi.jar
+export JARS=lib/helma.jar:lib/crimson.jar:lib/village.jar:lib/jsdk.jar
+export JARS=$JARS:lib/regexp.jar:lib/netcomponents.jar:lib/jimi.jar
 export JARS=$JARS:lib/mail.jar:lib/activation.jar:lib/mysql.jar:lib/jdom.jar:lib/minml.jar
 
 java -classpath $CLASSPATH:$JARS helma.main.Server -w $HOP_PORT
