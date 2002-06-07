0a09f6e2ed917e00537a3c7623ae99e62ee9fc9e
hns <hannesw@gmail.com>
2002-06-07 20:19:08 +0000

    updated version string

--- src/helma/main/Server.java
+++ src/helma/main/Server.java
@@ -23,7 +23,7 @@ import helma.util.*;
  
  public class Server implements IPathElement, Runnable {
 
-    public static final String version = "1.2pre3 2002/05/15";
+    public static final String version = "1.2pre3 2002/06/07";
     public static final long starttime = System.currentTimeMillis();
 
     public static boolean useTransactions = true;
