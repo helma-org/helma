0fea66820e9eabbf91848d3dedcbcd03c58110ae
hns <hannesw@gmail.com>
2002-07-24 18:45:56 +0000

    Update to new extended start scripts from Antclick distribution. Including jetty.jar and servlet.jar into the classpath.

--- build/main/hop.sh
+++ build/main/hop.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
-# Stupid shell script for starting Hop with a JDK-like virtual machine.
+# Shell script for starting Helma with a JDK-like virtual machine.
 # Presumes that you have your classpath set.
 
-export HOP_PORT=8080 
+# set Helma TCP ports
+WEB_PORT=8080
+XMLRPC_PORT=8081
 
-export JARS=lib/helma.jar:lib/crimson.jar:lib/village.jar:lib/jsdk.jar
-export JARS=$JARS:lib/regexp.jar:lib/netcomponents.jar:lib/jimi.jar:lib/apache-dom.jar
-export JARS=$JARS:lib/mail.jar:lib/activation.jar:lib/mysql.jar:lib/jdom.jar:lib/minml.jar
+# if JAVA_HOME variable is set, use it. Otherwise, java executable
+# must be contained in PATH variable.
+if [  "$JAVA_HOME" ]; then
+    JAVACMD="$JAVA_HOME"/bin/java
+else
+   JAVACMD=java
+fi
 
-java -classpath $CLASSPATH:$JARS helma.main.Server -w $HOP_PORT
+# set classpath
+JARS=lib/helma.jar:lib/crimson.jar:lib/village.jar:lib/servlet.jar:lib/jetty.jar
+JARS=$JARS:lib/regexp.jar:lib/netcomponents.jar:lib/jimi.jar:lib/apache-dom.jar
+JARS=$JARS:lib/mail.jar:lib/activation.jar:lib/mysql.jar:lib/jdom.jar:lib/minml.jar
+
+echo Starting Web server on port $WEB_PORT
+echo Starting XML-RPC server on port $XMLRPC_PORT
+
+# launch
+$JAVACMD -classpath $CLASSPATH:$JARS helma.main.Server -w $WEB_PORT -x $XMLRPC_PORT
