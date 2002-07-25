68f516710caab922171d338842e22581f6fe2f96
hns <hannesw@gmail.com>
2002-07-25 12:09:35 +0000

    Improved start scripts, added jetty jar files.

--- build/antclick/hop.bat
+++ build/antclick/hop.bat
@@ -2,12 +2,17 @@
 
 rem Batch file for starting Hop with a JDK-like virtual machine.
 
-set JARS=lib\helma.jar;lib\crimson.jar;lib\village.jar;lib\jsdk.jar;lib\mckoidb.jar
+rem Set Helma TCP ports
+set WEB_PORT=8080
+set XMLRPC_PORT=8081
+
+rem Set Classpath
+set JARS=lib\helma.jar;lib\crimson.jar;lib\village.jar;lib\servlet.jar;lib\jetty.jar
+set JARS=%JARS%;lib\mckoidb.jar
 set JARS=%JARS%;lib\regexp.jar;lib\netcomponents.jar;lib\jimi.jar;lib\apache-dom.jar
 set JARS=%JARS%;lib\mail.jar;lib\activation.jar;lib\mysql.jar;lib\jdom.jar;lib\minml.jar
 
-set HOP_PORT=8080
+echo Starting Web server on port %WEB_PORT%
+echo Starting XML-RPC server on port %XMLRPC_PORT%
 
-echo Starting Web server on port %HOP_PORT%
-
-java -classpath %JARS% helma.main.Server -w %HOP_PORT%
+java -classpath %JARS% helma.main.Server -w %WEB_PORT% -x %XMLRPC_PORT%
