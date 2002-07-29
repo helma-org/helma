4b35b3bf2c202ec7424cb36f91e5a9cfa4314497
kmfdm <helma@helma.org>
2002-07-29 08:32:09 +0000

    added comment that advises initScripting, applicationStarted and applicationStopped to be synchronized.

--- src/helma/extensions/HelmaExtension.java
+++ src/helma/extensions/HelmaExtension.java
@@ -20,17 +20,23 @@ public abstract class HelmaExtension {
 
 	/**
 	  * called when an Application is started.
+	  * This should be synchronized. Every application starting calls this method. Otherwise this could
+	  * lead to problems when more than one application starts at the same time.
 	  */
 	public abstract void applicationStarted (Application app) throws ConfigurationException;
 
 	/**
 	  * called when an Application is stopped.
+	  * This should be synchronized. Otherwise this could lead to problems when more than one
+	  * application gets stopped.
 	  */
 	public abstract void applicationStopped (Application app);
 
 	/**
 	  * called by the ScriptingEngine when it is initizalized. throws a ConfigurationException
 	  * when this type of ScriptingEngine is not supported.
+	  * This should be synchronized. Every application starting calls this method. Otherwise this could
+	  * lead to problems when more than one application starts at the same time.
 	  */
 	public abstract void initScripting (Application app, ScriptingEngine engine) throws ConfigurationException;
 
