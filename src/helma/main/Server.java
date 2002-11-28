765b1dddf8bb9f02211534209f8ca241f8c9c4d5
hns <hannesw@gmail.com>
2002-11-28 18:23:05 +0000

    Updated version string

--- src/helma/main/Server.java
+++ src/helma/main/Server.java
@@ -25,7 +25,7 @@ import org.apache.xmlrpc.*;
 
  public class Server implements IPathElement, Runnable {
 
-    public static final String version = "1.2pre4 2002/11/22";
+    public static final String version = "1.2pre4 2002/11/28";
     public final long starttime;
 
     // if true we only accept RMI and XML-RPC connections from 
