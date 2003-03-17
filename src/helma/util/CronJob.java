eac15c2d88ee22bf70d273d4d9c3becce36c7ff2
stefanp <helma@helma.org>
2003-03-17 15:12:15 +0000

    - added static parseTimeout() which transfers String to Long, so that   setTimeout() takes a Long value as input - CronJob.parse() now returns a Vector that is sorted (shortest timeout first) - added toString() - added utility-methods to compute millis until next full minute

--- src/helma/util/CronJob.java
+++ src/helma/util/CronJob.java
@@ -86,7 +86,7 @@ public class CronJob {
 
    private String name     = null;
    private String function = null;
-   private long   timeout = 30000;
+   private long   timeout = 20000;
 
    /** A method for parsing properties. It looks through the properties
      * file for entries that look like this:
@@ -161,7 +161,7 @@ public class CronJob {
      */
 
 
-  public static Collection parse(Properties props) {
+  public static Vector parse(Properties props) {
       Hashtable jobs = new Hashtable ();
       Enumeration e = props.keys ();
       while (e.hasMoreElements ()) {
@@ -195,12 +195,36 @@ public class CronJob {
                parseHour (job, value);
             } else if (jobSpec.equalsIgnoreCase("minute")) {
                parseMinute (job, value);
+            } else if (jobSpec.equalsIgnoreCase("timeout")) {
+            	parseTimeout (job, value);
             }
          } catch (NoSuchElementException nsee) {
          }
       }
-      return jobs.values ();
+      Vector jobVec = new Vector (jobs.values ());
+      Collections.sort (jobVec, new Comparator() {
+        public int compare (Object o1, Object o2) {
+            CronJob cron1 = (CronJob) o1;
+            CronJob cron2 = (CronJob) o2;
+            if (cron1.getTimeout () > cron2.getTimeout ())
+                return 1;
+            else if (cron1.getTimeout () < cron2.getTimeout ())
+                return -1;
+            else
+                return 0;
+        }
+        public boolean equals (Object o1, Object o2) {
+            if (o1!=null) {
+                return o1.equals (o2);
+            } else {
+                return false;
+            }
+        }
+
+        });
+      return jobVec;
    }
+
 
 
    public static void parseYear (CronJob job, String value) {
@@ -351,6 +375,23 @@ public class CronJob {
    }
 
 
+   public static void parseTimeout (CronJob job, String timeout) {
+      long timeoutValue = 1000 * Long.valueOf(timeout).longValue ();
+      job.setTimeout (timeoutValue);
+      System.out.println("parseTimeout(), value is now " + job.getTimeout ());
+   }
+
+	public static long nextFullMinute () {
+		long now = System.currentTimeMillis();
+      long millisAfterMinute = (now % 60000);
+		return (now + 60000 - millisAfterMinute);
+	}
+
+	public static long millisToNextFullMinute () {
+		long now = System.currentTimeMillis();
+      long millisAfterMinute = (now % 60000);
+		return (60000 - millisAfterMinute);
+	}
 
   /**
    *  Create an empty CronJob.
@@ -657,13 +698,6 @@ public class CronJob {
     this.timeout = timeout;
   }
 
-  /**
-   *  Set this entry's timeout
-   */
-  public void setTimeout(String timeout)
-  {
-    this.timeout = Long.valueOf(timeout).longValue ();
-  }
 
   /**
    *  Get this entry's timeout
@@ -672,4 +706,9 @@ public class CronJob {
   {
     return this.timeout;
   }
+  
+  public String toString () {
+  		return "[CronJob " + name + "]";
+	}
+  
 }
