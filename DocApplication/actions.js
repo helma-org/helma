08d3a944295a5406405c6598d08015502201e0d8
stefanp <helma@helma.org>
2004-01-06 15:47:22 +0000

    moved the actual rendering part in render_action into functions.js

--- DocApplication/actions.js
+++ DocApplication/actions.js
@@ -46,29 +46,13 @@ function render_action () {
    // set res.data.rendering, this will suppress the link back to the manage
    // console in the apidocs actions
    res.data.rendering = true;
-	if (checkAddress()==false)
-	   return;
-	if (checkAuth(this.getParentElement ())==false)
-	   return;
-	var prefix = this.href ("");
-	this.storePage (this, "main", "", "index.html");
-	this.storePage (this, "prototypes");
-	this.storePage (this, "summary");
-	this.storePage (this, "functionindex");
-	var ct = 4;
-	var arr = this.listChildren ();
-	for (var i=0; i<arr.length; i++) {
-		this.storePage (arr[i], "list", "../");
-		this.storePage (arr[i], "main", "../");
-		ct += 2;
-		var subarr = arr[i].listChildren ();
-		for (var j=0; j<subarr.length; j++) {
-			this.storePage (subarr[j], "main", "../", subarr[j].getElementName () + ".html");
-			ct += 1;
-		}
-	}
-	res.data.body =  '<body>rendering API ...<br/>wrote ' + ct + ' files<br/><br/>';
-	res.data.body += '<a href="' + root.href ("main") + '">back to manage console</a>';
+   if (checkAddress()==false)
+      return;
+   if (checkAuth(this.getParentElement ())==false)
+      return;
+   var ct = this.renderApi();
+   res.data.body =  '<body>rendering API ...<br/>wrote ' + ct + ' files<br/><br/>';
+   res.data.body += '<a href="' + root.href ("main") + '">back to manage console</a>';
    res.data.title = "rendering helma api";
    res.data.head = renderSkinAsString("head");
    renderSkin ("basic");
