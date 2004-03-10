aa9cc97c073fd199a4815c7883bcae4bd01cb268
hns <hannesw@gmail.com>
2004-03-10 15:35:23 +0000

    Update version to 1.4.0-rc3

--- src/helma/main/Server.java
+++ src/helma/main/Server.java
@@ -41,7 +41,7 @@ import java.util.*;
  */
 public class Server implements IPathElement, Runnable {
     // version string
-    public static final String version = "1.4.0-rc2 (2004/02/20)";
+    public static final String version = "1.4.0-rc3 (2004/03/10)";
 
     // static server instance
     private static Server server;
