2dcc4ae9cd91fcaa4b258f12d4b5651437ff3b34
hns <hannesw@gmail.com>
2005-10-21 15:28:01 +0000

    * Trim down compareTo() and equals() implementations (hopefully not breaking anything).

--- src/helma/objectmodel/db/Property.java
+++ src/helma/objectmodel/db/Property.java
@@ -486,78 +486,49 @@ public final class Property implements IProperty, Serializable, Cloneable, Compa
 
     /**
      * @see java.lang.Comparable#compareTo(java.lang.Object)
-     * FIXME: throw a ClassCastException instead?
-     * The following cases throw a RuntimeException
+     *
+     * The following cases throw a ClassCastException
      * - Properties of a different type
-     * - Properties of boolean type
+     * - Properties of boolean or node type
      */
     public int compareTo(Object obj) {
         Property p = (Property) obj;
-        int pt = p.getType();
-        
-        if (type==NODE || pt==NODE)
-            throw new RuntimeException ("unable to compare nodes " + this + " : " + p);
-        if (value==null && p.getStringValue() == null)
-            return 0;
-        if (value==null)
-            return -1;
-        if (p.getStringValue()==null)
-            return 1;
-        if (pt != type)
-            throw new RuntimeException ("uncomparable values " + this + "(" + type + ") : " + p + "(" + pt + ")");
-        switch (pt) {
-            case Property.STRING:
-                return (this.getStringValue().compareTo(p.getStringValue()));
-            case Property.INTEGER:
-                long l = this.getIntegerValue() - p.getIntegerValue();
-                // don't know what happens if the result of the subtraction
-                // has a higher value than the value which may be stored inside
-                // of an integer.
-                if (l < 0)
-                    return -1;
-                if (l > 0)
-                    return 1;
-                return 0;
-            case Property.DATE:
-                return this.getDateValue().compareTo(p.getDateValue());
-            case Property.FLOAT:
-                double d = this.getFloatValue() - p.getFloatValue();
-                if (d < 0)
-                    return -1;
-                if (d > 0)
-                    return 1;
-                return 0;
-            case Property.BOOLEAN:
-                throw new RuntimeException ("unable to compare boolean " + this + " : " + p);
-            case Property.JAVAOBJECT:
+        int ptype = p.getType();
+        Object pvalue = p.getValue();
+
+        if (type==NODE || ptype==NODE ||
+                type == BOOLEAN || ptype == BOOLEAN ||
+                type != ptype) {
+            throw new ClassCastException("uncomparable values " + this + "(" + type + ") : " + p + "(" + ptype + ")");
         }
-        throw new RuntimeException ("uncomparable values " + this + " : " + p);
+        if (value==null && pvalue == null) {
+            return 0;
+        } else if (value == null) {
+            return -1;
+        } if (pvalue == null) {
+            return 1;
+        }
+        if (!(value instanceof Comparable)) {
+            throw new ClassCastException("uncomparable value " + this + "(" + this.getClass() + ")");
+        }
+        return ((Comparable) value).compareTo(pvalue);
     }
 
-    public boolean equals(Object o) {
-        if (o == this)
+    /**
+     * Return true if object o is equal to this property.
+     *
+     * @param obj the object to compare to
+     * @return true if this equals obj
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    public boolean equals(Object obj) {
+        if (obj == this)
             return true;
-        if (o == null)
+        if (obj == null)
             return false;
-        if (!(o instanceof Property))
+        if (!(obj instanceof Property))
             return false;
-        Property p = (Property) o;
-        switch (p.getType()) {
-            case Property.STRING:
-                return (this.getStringValue().equals(p.getStringValue()));
-            case Property.INTEGER:
-                return this.getIntegerValue() == p.getIntegerValue();
-            case Property.DATE:
-                return this.getDateValue().equals(p.getDateValue());
-            case Property.FLOAT:
-                return this.getFloatValue() == p.getFloatValue();
-            case Property.BOOLEAN:
-                return this.getBooleanValue() == p.getBooleanValue();
-            case Property.NODE:
-                return this.node.equals(p.node);
-            case Property.JAVAOBJECT:
-                return this.value.equals(p.value);
-        }
-        return false;
+        Property p = (Property) obj;
+        return value == null ? p.value == null : value.equals(p.value);
     }
 }
