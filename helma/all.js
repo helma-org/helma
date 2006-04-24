6daa6c079461b6ed83932aaa64ec566763c4594c
zumbrunn <chris@zumbrunn.com>
2006-04-24 08:27:00 +0000

    Added Helma license notice

--- helma/all.js
+++ helma/all.js
@@ -1,3 +1,18 @@
+/*
+ * Helma License Notice
+ *
+ * The contents of this file are subject to the Helma License
+ * Version 2.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://adele.helma.org/download/helma/license.txt
+ *
+ * Copyright 1998-2006 Helma Software. All Rights Reserved.
+ *
+ * $RCSfile: Aspects.js,v $
+ * $Author: czv $
+ * $Revision: 1.2 $
+ * $Date: 2006/04/24 07:02:17 $
+ */
 
 // convenience SingleFileRepository to load all the 
 // Javascript library files in ./modules/helma
