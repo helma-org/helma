ebf02d851d7f9fe24e9c13a3edbc6294b859dcf2
zumbrunn <chris@zumbrunn.com>
2006-04-24 07:18:45 +0000

    added all.js files as convenience SingleFileRepositories to load all the Javascript library files in modules/core and/or modules/helma

---
+++ core/all.js
@@ -0,0 +1,9 @@
+
+// convenience SingleFileRepository to load all the 
+// Javascript library files in ./modules/core
+
+app.addRepository('modules/helma/Array.js');
+app.addRepository('modules/helma/Date.js');
+app.addRepository('modules/helma/Number.js');
+app.addRepository('modules/helma/Object.js');
+app.addRepository('modules/helma/String.js');
---
+++ helma/all.js
@@ -0,0 +1,19 @@
+
+// convenience SingleFileRepository to load all the 
+// Javascript library files in ./modules/helma
+
+app.addRepository('modules/helma/Aspects.js');
+app.addRepository('modules/helma/Chart.js');
+app.addRepository('modules/helma/Color.js');
+app.addRepository('modules/helma/Database.js');
+app.addRepository('modules/helma/File.js');
+app.addRepository('modules/helma/Ftp.js');
+app.addRepository('modules/helma/Html.js');
+app.addRepository('modules/helma/Http.js');
+app.addRepository('modules/helma/Image.js');
+app.addRepository('modules/helma/Mail.js');
+app.addRepository('modules/helma/Search.js');
+app.addRepository('modules/helma/Skin.js');
+app.addRepository('modules/helma/Ssh.js');
+app.addRepository('modules/helma/Url.js');
+app.addRepository('modules/helma/Zip.js');
