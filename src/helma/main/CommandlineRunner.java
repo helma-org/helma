21dfa78b0b728eb4819255a8edc70464f536c0b1
hns <hannesw@gmail.com>
2007-03-28 13:49:43 +0000

    * Stop server when shutting down.

--- src/helma/main/CommandlineRunner.java
+++ src/helma/main/CommandlineRunner.java
@@ -105,9 +105,8 @@ public class CommandlineRunner {
             }
         }
 
-        // stop the application
-        server.stopApplication(appName);
-
+        // stop the application and server
+        server.stop();
     }
 
     
