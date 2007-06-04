8e82dfe5d9f36934bbe1154e41344d520e64785c
hns <hannesw@gmail.com>
2007-06-04 08:06:30 +0000

    * Fix license header from Apache 2 to Helma 2 license.

--- src/helma/framework/FutureResult.java
+++ src/helma/framework/FutureResult.java
@@ -1,17 +1,17 @@
 /*
- *  Copyright 2006 Hannes Wallnoefer <hannes@helma.at>
+ * Helma License Notice
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * The contents of this file are subject to the Helma License
+ * Version 2.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://adele.helma.org/download/helma/license.txt
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * Copyright 1998-2006 Helma Software. All Rights Reserved.
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * $RCSfile$
+ * $Author$
+ * $Revision$
+ * $Date$
  */
 
 package helma.framework;
