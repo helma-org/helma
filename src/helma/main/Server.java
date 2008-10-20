fb12fd3e4d657fb21c917569a48b57dc509b26fc
hns <hannesw@gmail.com>
2008-10-20 13:35:43 +0000

    Make location of db.properties customizable using the dbPropFile server property. Contributed by Bernhard Fröhlich. Fixes bug 640 <http://helma.org/bugs/show_bug.cgi?id=640>

--- src/helma/main/Server.java
+++ src/helma/main/Server.java
@@ -435,14 +435,21 @@ public class Server implements Runnable {
 
 
         // read db.properties file in helma home directory
+        String dbPropfile = sysProps.getProperty("dbPropFile");
+        File file;
+        if ((dbPropfile != null) && !"".equals(dbPropfile.trim())) {
+            file = new File(dbPropfile);
+        } else {
+            file = new File(hopHome, "db.properties");
+        }
+
         dbProps = new ResourceProperties();
         dbProps.setIgnoreCase(false);
-        dbProps.addResource(new FileResource(new File(hopHome, "db.properties")));
+        dbProps.addResource(new FileResource(file));
         DbSource.setDefaultProps(dbProps);
 
         // read apps.properties file
         String appsPropfile = sysProps.getProperty("appsPropFile");
-        File file;
         if ((appsPropfile != null) && !"".equals(appsPropfile.trim())) {
             file = new File(appsPropfile);
         } else {
