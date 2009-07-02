cfae14575651cfbd3b1e64a1640407cd35650a79
hns <hannesw@gmail.com>
2009-07-02 12:49:44 +0000

    Set version to 1.7.0-rc1

--- src/helma/main/Server.java
+++ src/helma/main/Server.java
@@ -38,7 +38,7 @@ import helma.util.ResourceProperties;
  */
 public class Server implements Runnable {
     // version string
-    public static final String version = "1.7.0 (__builddate__)";
+    public static final String version = "1.7.0-rc1 (__builddate__)";
 
     // static server instance
     private static Server server;
